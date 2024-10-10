851594d3677eed2cd60cd177a0a676ee4767072d
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-10-10 17:02:42 +0300

    feat: html morning and media

---
+++ src/js/sounds.js
@@ -0,0 +1,61 @@
+const sounds = [
+  [
+    {
+      id:0,
+      name: 'Morning sounds 1',
+      src: 'audio/Morning sounds/Morning sounds_1.mp3'
+    },
+    {
+      id:1,
+      name: 'Morning sounds 2',
+      src: 'audio/Morning sounds/Morning sounds_2.mp3'
+    },
+    {
+      id:2,
+      name: 'Morning sounds 3',
+      src: 'audio/Morning sounds/Morning sounds_3.mp3'
+    },
+    {
+      id:3,
+      name: 'Morning sounds 4',
+      src: 'audio/Morning sounds/Morning sounds_4.mp3'
+    },
+    {
+      id:4,
+      name: 'Morning sounds 5',
+      src: 'audio/Morning sounds/Morning sounds_5.mp3'
+    },
+    {
+      id:5,
+      name: 'Morning sounds 6',
+      src: 'audio/Morning sounds/Morning sounds_6.mp3'
+    },
+    {
+      id:6,
+      name: 'Morning sounds 7',
+      src: 'audio/Morning sounds/Morning sounds_7.mp3'
+    },
+    {
+      id:7,
+      name: 'Morning sounds 8',
+      src: 'audio/Morning sounds/Morning sounds_8.mp3'
+    },
+    {
+      id:8,
+      name: 'Morning sounds 9',
+      src: 'audio/Morning sounds/Morning sounds_9.mp3'
+    },
+    {
+      id:9,
+      name: 'Morning sounds 10',
+      src: 'audio/Morning sounds/Morning sounds_10.mp3'
+    },
+    {
+      id:10,
+      name: 'Morning sounds 11',
+      src: 'audio/Morning sounds/Morning sounds_11.mp3'
+    }
+  ]
+]
+
+export default sounds;
