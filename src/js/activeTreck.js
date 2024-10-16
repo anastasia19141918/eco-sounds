8d7b65a3fc253af51b8e79e3147fcd21d6b50b08
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-10-16 17:23:33 +0300

    feat: audio js

--- src/js/activeTreck.js
+++ src/js/activeTreck.js
@@ -1,9 +1,21 @@
+import sounds from './sounds.js';
+
 const audio = new Audio;
 let isPlay = false;
 let playNum = 0;
 
 const playerBtn = document.getElementById('player__btn_play');
 const playerTitle = document.getElementById('player__title');
+const playerRange = document.getElementById('player__range');
+
+const current = document.getElementById('current');
+const lenghtTime = document.getElementById('lenghtTime');
+
+playerBtn.addEventListener('click', playAudio);
+audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener('ended', playNext);
+audio.addEventListener('timeupdate', timeProgressBar);
+playerRange.addEventListener('change', setProgressPlay);
 
 export function showTreck(el) {
   el.forEach(function(e, index){
@@ -16,12 +28,14 @@ export function showTreck(el) {
  export function playAudio(e) {
   if(isPlay === false) {
     isPlay = true;
-    audio.src = e[playNum].src;
+    audio.src = sounds[0][playNum].src;
     audio.currentTime = 0;
     playerBtn.classList.add('player__btn_pause');
-    playerTitle.textContent = e[playNum].name;
+    playerTitle.textContent = sounds[0][playNum].name;
     audio.play();
-  } else {
+  } 
+  
+  else {
     isPlay = false;
     playerBtn.classList.remove('player__btn_pause');
     audio.pause();
@@ -31,29 +45,103 @@ export function showTreck(el) {
 export function playNext (el) {
   if(playNum === el.length-1) {
     playNum = 0;
-    playAudio();
+    saundsTreck() 
   } else {
     playNum++;
-    playAudio();
+    saundsTreck() 
   }
-  playerTitle.textContent = el[playNum].name;
+  playAudio();
 };
 
 export function playPref (el) {
   if(playNum === 0) {
     playNum = el.length-1;
-    playAudio();
+    saundsTreck()
   } else {
     playNum --;
-    playAudio();
+    saundsTreck()
   }
-  playerTitle.textContent = el[playNum].name;
+  playAudio();
 }
 
-function saundsTreck() {
+function saundsTreck(el) {
   if(isPlay === true) {
     isPlay = false;
     playerBtn.classList.toggle('player__btn_pause');
+    audio.play();
   }
 }
+
+//progress start
+
+export function updateProgress(e) {
+  playerRange.value = (e.target.currentTime / e.target.duration) * 100;
+  const value = playerRange.value;
+  playerRange.style.background = `linear-gradient(to right,  #ffffff 0%, #ffffff ${value}%, #c8c8c8 ${value}%, #c8c8c8  100%)`;
+}
+
+function setProgressPlay () {
+  audio.currentTime = (playerRange.value * audio.duration) / 100;
+  const value = playerRange.value;
+  playerRange.style.background = `linear-gradient(to right, #ffffff 0%, #ffffff ${value}%, #c8c8c8 ${value}%, #c8c8c8  100%)`;
+}
+
+//progress end
+
+//time progress bsr start
+
+function timeProgressBar () {
+  audio.value = (playerRange.value * audio.duration) / 100;
+  current.textContent = audioCurrent();
+  lenghtTime.textContent = audioLenght();
+}
+
+function audioCurrent() {
+  let min = Math.floor(audio.currentTime / 60);
+  if (min < 10) {
+    min = '0' + String(min);
+  }
+
+  let sec = Math.floor(audio.currentTime % 60);
+  if (sec < 10) {
+    sec = '0' + String(sec);
+  }
+
+  if (isNaN(min) || isNaN(sec)) {
+    min = '00';
+    sec = '00';
+  }
+
+  return `${min}:${sec}`;
+}
+
+function audioLenght () {
+  let min = Math.floor(audio.duration / 60);
+  if (min < 10) {
+    min = '0' + String(min);
+  }
+
+  let sec = Math.floor(audio.duration % 60);
+  if (sec < 10) {
+    sec = '0' + String(sec);
+  }
+
+  if (isNaN(min) || isNaN(sec)) {
+    min = '00';
+    sec = '00';
+  }
+
+  return `${min}:${sec}`;
+}
+
+//time progress bsr end
+
+
+
+
+
+
+
+
+
 
