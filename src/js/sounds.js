2e1b4121f66eab0dda15e70632561c8a77430b27
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-10-17 16:53:20 +0300

    feat: arrow bird

--- src/js/sounds.js
+++ src/js/sounds.js
@@ -57,7 +57,71 @@ const sounds = [
     }
   ],
   [
-    
+    {
+      id:0,
+      name: 'Birds Singing 1',
+      src: 'src/audio/Birds/Birds-singing_1.mp3'
+    },
+    {
+      id:1,
+      name: 'Birds Singing 2',
+      src: 'src/audio/Birds/Birds-singing_2.mp3'
+    },
+    {
+      id:2,
+      name: 'Birds Singing 3',
+      src: 'src/audio/Birds/Birds-singing_3.mp3'
+    },
+    {
+      id:3,
+      name: 'Birds Singing 4',
+      src: 'src/audio/Birds/Birds-singing_4.mp3'
+    },
+    {
+      id:4,
+      name: 'Birds Singing 5',
+      src: 'src/audio/Birds/Birds-singing_5.mp3'
+    },
+    {
+      id:5,
+      name: 'Birds Singing 6',
+      src: 'src/audio/Birds/Birds-singing_6.mp3'
+    },
+    {
+      id:6,
+      name: 'Birds Singing 7',
+      src: 'src/audio/Birds/Birds-singing_7.mp3'
+    },
+    {
+      id:7,
+      name: 'Birds Singing 8',
+      src: 'src/audio/Birds/Birds-singing_8.mp3'
+    },
+    {
+      id:8,
+      name: 'Birds Singing 9',
+      src: 'src/audio/Birds/Birds-singing_9.mp3'
+    },
+    {
+      id:9,
+      name: 'Birds Singing 10',
+      src: 'src/audio/Birds/Birds-singing_10.mp3'
+    },
+    {
+      id:10,
+      name: 'Birds Singing 11',
+      src: 'src/audio/Birds/Birds-singing_11.mp3'
+    },
+    {
+      id:11,
+      name: 'Birds Singing 12',
+      src: 'src/audio/Birds/Birds-singing_12.mp3'
+    }
+    {
+      id:12,
+      name: 'Birds Singing 13',
+      src: 'src/audio/Birds/Birds-singing_13.mp3'
+    }
   ]
 ]
 
